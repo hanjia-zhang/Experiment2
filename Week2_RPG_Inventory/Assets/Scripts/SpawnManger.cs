9ac225ebc6e441dada2f507710d3421107f4712d
hanjia-zhang <felixzhjcr7@gmail.com>
2022-05-18 14:58:48 -0400

    Experiment2

---
+++ Week2_RPG_Inventory/Assets/Scripts/SpawnManger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManger : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    private float spawnRangeX = 30;
+    private float spawnRangeY = 18;
+
+    private float startDelay = 2;
+    private float spawnInterval = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomEnemy", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomEnemy()
+    {
+        int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+        Vector2 spawnPos = new Vector2(Random.Range(-spawnRangeX, spawnRangeX), Random.Range(-spawnRangeY, spawnRangeY));
+        Instantiate(enemyPrefabs[enemyIndex], spawnPos, enemyPrefabs[enemyIndex].transform.rotation);
+    }
+}
---
+++ Week2_RPG_Inventory/Assets/Scripts/sunattact.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunattact : MonoBehaviour
+{
+    private PlayerController player;
+
+    public int hitpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D feedback)
+    {
+        if (feedback.name == "Player")
+        {
+            player.health  -= hitpoint;
+        }
+        Debug.Log("I'M bleeding!");
+    }
+}
